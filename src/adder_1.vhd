b79c7262d60d553a088e93c907bb242620e9d46c
AG-Elian <aramallog.elian@gmail.com>
2024-10-21 15:15:45 -0300

    Se corrigió adder_1 y .gitignore

--- src/adder_1.vhd
+++ src/adder_1.vhd
@@ -5,14 +5,14 @@ entity adder_1 is
   port (
     A : in  std_logic;
     B : in  std_logic;
-    Ci : in std_logic;
-    Y : out std_logic;
+    Ci : in std_logic; -- Carry de entrada
+    Y : out std_logic; -- Carry de salida
     Co : out  std_logic
   );
 end adder_1;
 
 architecture arch of adder_1 is
 begin
-  Y <= (A and B) or (B and Ci) or (A and Ci);
-  Co <= (A and B) or (Ci and (A xor B));
+  Y <= A xor B xor Ci; --Suma
+  Co <= (A and B) or (Ci and (A xor B)); -- Acarreo
 end arch;
