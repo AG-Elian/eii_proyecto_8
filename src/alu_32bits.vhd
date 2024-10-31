639cada589296708fb3508df7ccc8b44fc85d85c
Aramallo <aramallog.elian@gmail.com>
2024-10-31 16:47:37 -0300

    Se añaden modificaciones a la ALU, aun sin probarse. Se añaden modificaciones menores a fn_suma_resta.

--- src/alu_32bits.vhd
+++ src/alu_32bits.vhd
@@ -11,63 +11,68 @@ entity alu_32bits is
 end alu_32bits;
 
 architecture structural of alu_32bits is
-  component fn_cero32bits is
+  component fn_cero32bits is -- Componente cero de 32 bits
     port (
       A,B : in std_logic_vector(31 downto 0);
       Z   : out std_logic
     );
     end component;
   
-  component and_32 is
+  component and_32 is --Componente AND con entradas y salida de 32 bits
     port(
       A,B : in std_logic_vector(31 downto 0);
       Y   : out std_logic_vector(31 downto 0)
     );
     end component;
-  component or_32 is
+  component or_32 is --Función OR con entradas y salida de 32bits
     port(
       A,B : in std_logic_vector(31 downto 0);
       Y   : out std_logic_vector(31 downto 0)
     );
   end component;
-  component xor_32 is
+  component xor_32 is -- Componente XOR con entrada y salida de 32 bits
     port(
       A,B : in std_logic_vector(31 downto 0);
       Y   : out std_logic_vector(31 downto 0)
     );
     end component;
-  component desplaza_izq is
-    port(
-      A : in std_logic_vector(31 downto 0);
-      B : in std_logic_vector(4 downto 0);
-      Y : out std_logic_vector(31 downto 0)
-    );
-    end component;
-  component fn_suma_resta is
+  
+  component fn_suma_resta is -- Componente SUMA/RESTA con entrada A y B 32 bits y selección. Salida de 32 bits
     port(
       A, B : in std_logic_vector(31 downto 0);
       sel : in std_logic;
       Y : out std_logic_vector(31 downto 0)
     );
     end component;
-  component fn_menor_que is
+  component fn_menor_que is  -- Componente MENOR QUE con entradas de 32 bits y salida de 1 bit
     port(
       A, B : in std_logic_vector(31 downto 0);
       con_signo : in std_logic;
       Y : out std_logic
     );
     end component;
-  component desplaza_der_32bits is
+    component desplaza_izq is -- Componente DESPLAZAMIENTO A LA IZQUIERDA: ingresa ceros desde la izquierda
+      port(
+        A : in std_logic_vector(31 downto 0);
+        B : in std_logic_vector(4 downto 0);
+        Y : out std_logic_vector(31 downto 0)
+      );
+      end component;
+
+  component desplaza_der_32bits is -- Componente DESPLAZAMIENTO A LA DERECHA: ingresa ceros o unos desde la derecha
     port(
       A : in std_logic_vector(31 downto 0);
-      B : in std_logic_vector(3 downto 0);
+      B : in std_logic_vector(4 downto 0);
       con_signo : std_logic;
       Y : out std_logic_vector(31 downto 0)
     );
     end component;
-   -- y_and, y_or, y_xor, y_sr, y_desp_der, y_desp_izq : std_logic_vector(31 downto 0);
-  -- y_sel : std_logic_vector;
-  -- resta, menor, con_signo, desp_consigno: std_logic_vector;
+  signal y_and, y_or, y_xor, y_sr, y_desp_der, y_desp_izq : std_logic_vector(31 downto 0);
+  signal y_sel : std_logic_vector(4 downto 0);
+  signal resta, menor_con_signo, desp_consigno : std_logic;
 begin
-  Y <= A and B;
+  U1 : fn_suma_resta port map(
+    A=>A, B=>B, resta=>resta, Y=>y_suma_resta);
+  U2 : fn_menor_que port map(
+    A=>A, B=>B, con_signo=>menor_consigno, Y=>y_menor);
 end structural;
--- src/fn_suma_resta.vhd
+++ src/fn_suma_resta.vhd
@@ -4,12 +4,12 @@ use IEEE.numeric_std.all;
 
 entity fn_suma_resta is
   port (
-    A : in  std_logic_vector(31 downto 0);
-    B : in  std_logic_vector(31 downto 0);
-    AmasB : out std_logic_vector(31 downto 0);
+    A       : in  std_logic_vector(31 downto 0);
+    B       : in  std_logic_vector(31 downto 0);
+    AmasB   : out std_logic_vector(31 downto 0);
     AmenosB : out std_logic_vector(31 downto 0);
-    sel : in std_logic;
-    Y : out std_logic_vector(31 downto 0)
+    sel     : in std_logic;
+    Y       : out std_logic_vector(31 downto 0)
   );
 end fn_suma_resta;
 
